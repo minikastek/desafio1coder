bc6c75a4ebe1bd55a0a891984409b6b5af43df1d
minikastek <97316967+minikastek@users.noreply.github.com>
2022-07-17 20:29:08 -0300

    Desafio 2
    Creando Navbar con logo y botones clickeables

---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,10 @@
+import './ItemListContainer.css'
+import React from 'react'
+
+export const ItemListContainer = ({greetings}) => {
+  return (
+    <div>
+        <h1>{greetings}</h1>
+    </div>
+  )
+}
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Button } from '../Button/Button';
+import { CartWidget } from '../CartWidget/CartWidget';
+import './Navbar.css';
+import name from "../../Assets/Name.png";
+
+export const Navbar = () => {
+
+    const text = 'hice click'
+    const handleClick= () =>{
+        return console.log(text);
+    }
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        
+        <div className="navbar-brand" style={{paddingLeft: 10}}>
+            <img src={name} className="logo"/>
+        </div>
+
+        <div className="buttonList">
+            <Button handleClick={handleClick} label='Cartas'/>
+            <Button label='Carpetas'/>
+            <Button label='Deckbox'/> 
+            <Button label='Sellados'/> 
+        </div>
+
+        {/* <div>
+            <CartWidget/>
+        </div> */}
+
+        {/* <div>
+            <button 
+                className='btn btn-primary'
+                style={{margin: 10, padding: 9}}
+            >
+                login
+            </button>
+
+            <button 
+                className='btn btn-primary'
+                style={{margin: 10, padding: 9}}
+            >
+                sign In
+            </button>
+        </div> */}
+
+    </nav>
+  )
+}
